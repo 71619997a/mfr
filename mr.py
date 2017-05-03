bc45e62600c1122aacf2724f210e6e4faf5b4dab
71619997a <gmarks@stuy.edu>
2017-05-02 22:30:17 -0400

    finish

--- mr.py
+++ mr.py
@@ -5,17 +5,22 @@ def getWords(filename):
         spl = re.sub('[^\w]', ' ', f.read())
     return re.sub('[ \t\n]+', ' ', spl).split()
 
-
 def frequency(word, words):
     return len(filter(lambda w: w == word, words))
 
 def frequencies(wordgroup, words):
     return len(filter(lambda w: w in wordgroup, words))
 
-def mostfrequent(wordgroup, words):
+def mostfrequent(words):
     def biggest(a, b):
         if a[1] > b[1]:
             return a
         return b
-    return reduce(biggest, map(lambda a: (a, frequency(a, words)), set(words)))
+    return reduce(biggest, map(lambda a: (a, frequency(a, words)), set(words)))[0]
 
+if __name__ == '__main__':
+    words = getWords('book.txt')
+    print 'Frequency of me:', frequency('me', words)
+    print 'Total frequency of me, him, and her:', frequencies(['me', 'him', 'her'], words)
+    mfw = mostfrequent(words)  # xd
+    print 'Most frequent word: %s, frequency: %d' % (mfw, frequency(mfw, words))
